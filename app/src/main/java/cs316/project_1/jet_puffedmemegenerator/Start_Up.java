8d56c8f2c8d963107111760467bca852237724ac
Dcb426 <Dcb13a@acu.edu>
2018-02-07 16:14:22 -0600

    Initial commit

---
+++ app/src/main/java/cs316/project_1/jet_puffedmemegenerator/Start_Up.java
@@ -0,0 +1,13 @@
+package cs316.project_1.jet_puffedmemegenerator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Start_Up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start__up);
+    }
+}
